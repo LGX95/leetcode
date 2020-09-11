4fcdedfb0148b7be2fb1ee695fe4c2d8287fc50f
LGX95 <ligengxin95@gmail.com>
2020-09-11 21:31:53 +0800

    Add, 21 Merge Two Sorted Lists

---
+++ 21-merge-two-sorted-lists.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return f'{self.val} -> {self.next}'
+
+    @classmethod
+    def from_list(cls, array):
+        curr = dummy = cls()
+        for i in array:
+            curr.next = cls(i)
+            curr = curr.next
+        return dummy.next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        curr = dummy = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        return dummy.next
+
+
+if __name__ == '__main__':
+    l1 = ListNode.from_list([1, 2, 4])
+    l2 = ListNode.from_list([1, 3, 4])
+    print(f'l1: {l1}\nl2: {l2}')
+    output = Solution().mergeTwoLists(l1, l2)
+    print(f'output: {output}\n')
