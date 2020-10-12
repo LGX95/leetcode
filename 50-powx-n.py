473de8fa46ac40b89ffc7e270e069e4af411c9d6
LGX95 <ligengxin95@gmail.com>
2020-10-12 14:05:50 +0800

    Add, Pow(x, n) binary multiple

--- 50-powx-n.py
+++ 50-powx-n.py
@@ -1,13 +1,49 @@
 """Question: https://leetcode.com/problems/powx-n/
 """
 
+from util import print_vars
+
 
 class Solution:
+    def my_pow_binary(self, x: float, n: int) -> float:
+        if x == 0.0:
+            return 0.0
+        if n < 0:
+            x, n = 1 / x, -n
+        res = 1
+        while n:
+            if n % 2 == 1:
+                res *= x
+            x *= x
+            n //= 2
+        return res
+
+    def my_pow_binary_bit(self, x: float, n: int) -> float:
+        if x == 0.0:
+            return 0.0
+        if n < 0:
+            x, n = 1 / x, -n
+        res = 1
+        while n:
+            if n & 1:
+                res *= x
+            x *= x
+            n >>= 1
+        return res
+
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+        return x**n
 
 
 if __name__ == '__main__':
     x, n = 2.0, 10
-    output= Solution().myPow(x, n)
-    print(f'x: {x} n: {n} output: {output}')
+    output = Solution().myPow(x, n)
+    print_vars(x, n, output)
+
+    x, n = 2.0, 10
+    output = Solution().my_pow_binary(x, n)
+    print_vars(x, n, output)
+
+    x, n = 2.0, 10
+    output = Solution().my_pow_binary_bit(x, n)
+    print_vars(x, n, output)
