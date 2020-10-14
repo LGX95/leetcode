14640481ef9d30d7403fbe296ccec5d3ee363566
LGX95 <ligengxin95@gmail.com>
2020-10-14 21:18:56 +0800

    Opt, 283 Move Zeroes, save non num to tmp

--- 283-move-zeroes.py
+++ 283-move-zeroes.py
@@ -6,6 +6,14 @@ from util import print_vars
 
 
 class Solution:
+    def move_zeros_save_non_zero(self, nums: List[int]) -> None:
+        tmp = [n for n in nums if n != 0]
+        for i in range(len(nums)):
+            if i < len(nums):
+                nums[i] = tmp[i]
+            else:
+                nums[i] = 0
+
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -22,4 +30,9 @@ if __name__ == "__main__":
     nums = [0, 1, 0, 3, 12]
     print_vars(nums)
     Solution().moveZeroes(nums)
+    print_vars(nums)
+
+    nums = [0, 1, 0, 3, 12]
+    print_vars(nums)
+    Solution().move_zeros_save_non_zero(nums)
     print_vars(nums)
