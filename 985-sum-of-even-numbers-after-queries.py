0ff4ca1ff151a90b57451b041dfba6b377e85954
LGX95 <ligengxin95@gmail.com>
2020-10-14 22:39:48 +0800

    Opt, 985 Sum of Even Numbers After Queries

--- 985-sum-of-even-numbers-after-queries.py
+++ 985-sum-of-even-numbers-after-queries.py
@@ -11,14 +11,11 @@ class Solution:
         res = []
         sum_even = sum(i for i in A if i % 2 == 0)
         for val, index in queries:
-            previous = A[index]
+            if A[index] % 2 == 0:
+                sum_even -= A[index]
             A[index] += val
-            if A[index] % 2 == 0 and previous % 2 != 0:
+            if A[index] % 2 == 0:
                 sum_even += A[index]
-            if A[index] % 2 == 0 and previous % 2 == 0:
-                sum_even += val
-            if A[index] % 2 != 0 and previous % 2 == 0:
-                sum_even -= previous
             res.append(sum_even)
         return res
 
