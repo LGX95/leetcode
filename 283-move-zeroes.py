aea4fce5e54862cb57a0723fca2b775b8295dd40
LGX95 <ligengxin95@gmail.com>
2020-10-14 21:28:27 +0800

    Add, 283 Move Zeroes, two pointer

--- 283-move-zeroes.py
+++ 283-move-zeroes.py
@@ -6,10 +6,21 @@ from util import print_vars
 
 
 class Solution:
+    def move_zeros_two_pointer(self, nums: List[int]) -> None:
+        slow = fast = 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            if nums[slow] != 0:
+                slow += 1
+            fast += 1
+
     def move_zeros_save_non_zero(self, nums: List[int]) -> None:
+        """currently fastest
+        """
         tmp = [n for n in nums if n != 0]
         for i in range(len(nums)):
-            if i < len(nums):
+            if i < len(tmp):
                 nums[i] = tmp[i]
             else:
                 nums[i] = 0
@@ -35,4 +46,9 @@ if __name__ == "__main__":
     nums = [0, 1, 0, 3, 12]
     print_vars(nums)
     Solution().move_zeros_save_non_zero(nums)
+    print_vars(nums)
+
+    nums = [0, 1, 0, 3, 12]
+    print_vars(nums)
+    Solution().move_zeros_two_pointer(nums)
     print_vars(nums)
