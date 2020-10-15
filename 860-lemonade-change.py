222c5752c817c13fa128ead5203951ab9b9d8483
LGX95 <ligengxin95@gmail.com>
2020-10-15 21:50:36 +0800

    Opt, 860 Lemonade Change, cleaner code

--- 860-lemonade-change.py
+++ 860-lemonade-change.py
@@ -8,25 +8,21 @@ from util import print_vars
 
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        in_hand = {5: 0, 10: 0, 20: 0}
+        five, ten = 0, 0
         for i in bills:
             if i == 5:
-                in_hand[i] += 1
+                five += 1
             elif i == 10:
-                if in_hand[5] < 1:
-                    return False
-                in_hand[5] -= 1
-                in_hand[10] += 1
-            elif i == 20:
-                if in_hand[5] > 0 and in_hand[10] > 0:
-                    in_hand[5] -= 1
-                    in_hand[10] -= 1
-                    in_hand[20] += 1
-                elif in_hand[5] >= 3:
-                    in_hand[5] -= 3
-                    in_hand[20] += 1
-                else:
-                    return False
+                five -= 1
+                ten += 1
+            elif ten > 0:  # i = 20
+                five -= 1
+                ten -= 1
+            else:  # i = 20 and no 10 in hand
+                five -= 3
+
+            if five < 0:
+                return False
         return True
 
 
